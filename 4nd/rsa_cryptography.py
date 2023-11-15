4ebb378b290ba09b03b57249f7af9b00a8642b0e
Sakai <sakai@VLAN4-dhcp32.is.env.kitakyu-u.ac.jp>
2023-11-15 23:55:36 +0900

    feature/pre_rsa

---
+++ 4nd/rsa_cryptography.py
@@ -0,0 +1,15 @@
+#モジュール
+import sys
+
+class RSA_Cryptography():
+    def __init__(self,p,q,e):
+        self.n = p * q
+        self.e = e
+    
+    def encryption(self):
+        list_q = []
+        #あまりが1になるまで繰り返す
+        while(r == 1){
+            q,r = divmod()
+        }
+        divmod(self.n,self.e)
